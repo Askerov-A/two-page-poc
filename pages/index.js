323d520390d70555ee34218a7796a49d8f9d928a
Azat Askerov <Azat_Askerov@epam.com>
2021-01-26 13:12:42 +0600

    Use getInitialProps instead of getServerSideProps

--- pages/index.js
+++ pages/index.js
@@ -1,43 +1,20 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import styles from '../styles/Home.module.css'
 import {getItems, URL} from "../api/items";
 
 export const itemStyles = { border: '1px solid black', width: 400, margin: 10, textAlign: 'center' };
 
 export default function Home(props) {
-  const [data, setData] = useState(props.data);
-
-  useEffect(() => {
-    if (props.makeRequestOnClientPart) {
-        getItems(URL).then(data => {
-          setData(data);
-          console.log(data);
-        })
-    }
-  }, []);
-
   return (
     <div className={styles.container}>
-        {data?.map((item) => (<div style={itemStyles} key={data.id}><img src={item.image} /><h1>{item.name}</h1></div>))}
+        {props.data?.map((item) => (<div style={itemStyles} key={item.id}><img src={item.image} /><h1>{item.name}</h1></div>))}
     </div>
   )
 }
 
-export async function getServerSideProps(context) {
-    const isServer = context.req.url === '/';
-
-    if (isServer) {
-        const data = await getItems(URL);
-        return {
-            props: {
-                data,
-            },
-        }
-    } else {
-        return {
-            props: {
-                makeRequestOnClientPart: true,
-            }
-        }
+Home.getInitialProps = async () => {
+    const data = await getItems(URL);
+    return {
+        data,
     }
 }
--- pages/popular.js
+++ pages/popular.js
@@ -1,41 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import styles from '../styles/Home.module.css'
-import {getItems, POPULAR_URL} from "../api/items";
+import {getItems, POPULAR_URL, URL} from "../api/items";
 import {itemStyles} from "./index";
 
-export default function Home(props) {
-    const [data, setData] = useState(props.data);
-
-    useEffect(() => {
-        if (props.makeRequestOnClientPart) {
-            getItems(POPULAR_URL).then(data => {
-                setData(data);
-            })
-        }
-    }, []);
-
+function Popular(props) {
     return (
         <div className={styles.container}>
-            {data?.map((item) => (<div style={itemStyles} key={data.id}><img src={item.image} /><h1>{item.name}</h1></div>))}
+            {props.data?.map((item) => (<div style={itemStyles} key={item.id}><img src={item.image} /><h1>{item.name}</h1></div>))}
         </div>
     )
 }
 
-export async function getServerSideProps(context) {
-    const isServer = context.req.url === '/popular';
-
-    if (isServer) {
-        const data = await getItems(POPULAR_URL);
-        return {
-            props: {
-                data,
-            },
-        }
-    } else {
-        return {
-            props: {
-                makeRequestOnClientPart: true,
-            }
-        }
+Popular.getInitialProps = async () => {
+    const data = await getItems(POPULAR_URL);
+    return {
+        data,
     }
 }
+
+export default Popular;
