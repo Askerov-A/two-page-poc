a5983c35ac6192adda366ce543dd67302051ea07
Azat Askerov <Azat_Askerov@epam.com>
2021-01-26 14:28:59 +0600

    Add counter

--- api/items.js
+++ api/items.js
@@ -1,6 +1,7 @@
 export const URL = 'http://localhost:8081/items';
 export const POPULAR_URL = 'http://localhost:8081/popular';
 export const STATIC_DATA_URL = 'http://localhost:8081/static-data';
+export const INDIVIDUAL_URL = 'http://localhost:8081/data';
 
 export const getItems = async (url) => {
     console.log("WE ARE DOING A REQUEST");
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from '../styles/Home.module.css'
-import {getItems, URL} from "../api/items";
+import {getItems, INDIVIDUAL_URL, URL} from "../api/items";
 
 export const itemStyles = { border: '1px solid black', width: 400, margin: 10, textAlign: 'center' };
 
 export default function Home(props) {
+  const [indData, setIndData] = useState(props.indData);
+
+  useEffect(() => {
+    getItems(INDIVIDUAL_URL).then(data => setIndData(data));
+  }, []);
+
   return (
     <div className={styles.container}>
-        {props.data?.map((item) => (<div style={itemStyles} key={item.id}><img src={item.image} /><h1>{item.name}</h1></div>))}
+        <h1 style={{ marginBottom: 0 }}>Counter: {indData?.counter}</h1>
+        <p style={{ marginBottom: 30, marginTop: 10 }}>(it updates twice because of prerender + client side fetch)</p>
+        {props.data?.map((item) => (<div style={itemStyles} key={item.id}><img src={item.image} /><h2>{item.name}</h2></div>))}
     </div>
   )
 }
 
 Home.getInitialProps = async () => {
     const data = await getItems(URL);
+    const indData = await getItems(INDIVIDUAL_URL);
+
     return {
         data,
+        indData,
     }
 }
